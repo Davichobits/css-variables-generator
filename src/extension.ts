b5788e528a0bcfeee00029bf71004de0b67f1be5
Davichobits <davidruizdeveloper@gmail.com>
2025-02-27 11:00:00 -0500

    feat: support for tailwind

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,12 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('extension.transformToCSSVars', () => {
+  // Comando para transformar a variables CSS
+  let disposableCSS = vscode.commands.registerCommand('extension.transformToCSSVars', () => {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
       return;
     }
-
-    console.log('TESTING');
 
     const selection = editor.selection;
     const text = editor.document.getText(selection);
@@ -19,7 +18,24 @@ export function activate(context: vscode.ExtensionContext) {
     });
   });
 
-  context.subscriptions.push(disposable);
+  // Comando para transformar a variables Tailwind
+  let disposableTailwind = vscode.commands.registerCommand('extension.generateTailwindVars', () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const selection = editor.selection;
+    const text = editor.document.getText(selection);
+
+    const transformedText = transformToTailwindVariables(text);
+
+    editor.edit(editBuilder => {
+      editBuilder.replace(selection, transformedText);
+    });
+  });
+
+  context.subscriptions.push(disposableCSS, disposableTailwind);
 }
 
 function transformToCSSVariables(text: string): string {
@@ -32,7 +48,6 @@ function transformToCSSVariables(text: string): string {
 
       // Eliminar paréntesis y reemplazar espacios por guiones
       name = name.replace(/\s*\(\s*(.*?)\s*\)\s*/g, '-$1').replace(/\s+/g, '-');
-      console.log(name);
 
       return `  --${name}: ${value};`;
     }
@@ -42,4 +57,23 @@ function transformToCSSVariables(text: string): string {
   return `:root {\n${vars.join('\n')}\n}`;
 }
 
-export function deactivate() {}
+function transformToTailwindVariables(text: string): string {
+  const lines = text.split('\n').map(line => line.trim()).filter(line => line);
+  const vars = lines.map(line => {
+    const match = line.match(/- (.+): (.+)/);
+    if (match) {
+      let name = match[1];
+      const value = match[2];
+
+      // Eliminar paréntesis y reemplazar espacios por guiones
+      name = name.replace(/\s*\(\s*(.*?)\s*\)\s*/g, '-$1').replace(/\s+/g, '-');
+      // Convertir a formato Tailwind
+      name = `color-${name.replace(/-/g, '-')}`;
+
+      return `  --${name}: ${value};`;
+    }
+    return '';
+  });
+
+  return `@theme {\n${vars.join('\n')}\n}`;
+}
